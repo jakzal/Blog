74f57addccde29a3d4af13550b6200d8214e576a
Jakub Zalas <jakub@zalas.pl>
2011-07-07 22:42:20 +0200

    Updated Symfony from 2beta2 to 2RC4. Updated Behat to 2.0RC2.

--- app/autoload.php
+++ app/autoload.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
@@ -19,7 +20,7 @@ $loader->registerNamespaces(array(
     'Behat\Behat'      => __DIR__.'/../vendor/behat/Behat/src',
     'Behat\Gherkin'    => __DIR__.'/../vendor/behat/Gherkin/src',
     'PSS'              => __DIR__.'/../src',
-    'Knplabs'          => __DIR__.'/../vendor/bundles',
+    'Knp\Bundle'       => __DIR__.'/../vendor/bundles',
     'Zend'             => __DIR__.'/../vendor/Zend/library'
 ));
 $loader->registerPrefixes(array(
@@ -33,6 +34,12 @@ $loader->registerNamespaceFallbacks(array(
     __DIR__.'/../src',
 ));
 $loader->register();
+
+AnnotationRegistry::registerLoader(function($class) use ($loader) {
+    $loader->loadClass($class);
+    return class_exists($class, false);
+});
+AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
 // Swiftmailer needs a special autoloader to allow
 // the lazy loading of the init file (which is expensive)
