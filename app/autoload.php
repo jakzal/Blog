085331ebdd49e6ac3ec37360551b02cdb3c07f22
Jakub Zalas <jakub@zalas.pl>
2011-12-17 22:22:32 +0000

    Added CommonContexts to deps.

--- app/autoload.php
+++ app/autoload.php
@@ -5,23 +5,24 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
-    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/../vendor/monolog/src',
-    'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Metadata'         => __DIR__.'/../vendor/metadata/src',
-    'Behat\Mink'       => __DIR__.'/../vendor/behat/Mink/src',
-    'Behat\MinkBundle' => __DIR__.'/../vendor/bundles',
-    'Behat\BehatBundle'=> __DIR__.'/../vendor/bundles',
-    'Behat\Behat'      => __DIR__.'/../vendor/behat/Behat/src',
-    'Behat\Gherkin'    => __DIR__.'/../vendor/behat/Gherkin/src',
-    'PSS'              => __DIR__.'/../src',
-    'Knp\Bundle'       => __DIR__.'/../vendor/bundles',
-    'Zend'             => __DIR__.'/../vendor/Zend/library'
+    'Symfony'             => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Sensio'              => __DIR__.'/../vendor/bundles',
+    'JMS'                 => __DIR__.'/../vendor/bundles',
+    'Doctrine\\Common'    => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\DBAL'      => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine'            => __DIR__.'/../vendor/doctrine/lib',
+    'Monolog'             => __DIR__.'/../vendor/monolog/src',
+    'Assetic'             => __DIR__.'/../vendor/assetic/src',
+    'Metadata'            => __DIR__.'/../vendor/metadata/src',
+    'Behat\Mink'          => __DIR__.'/../vendor/behat/Mink/src',
+    'Behat\MinkBundle'    => __DIR__.'/../vendor/bundles',
+    'Behat\BehatBundle'   => __DIR__.'/../vendor/bundles',
+    'Behat\Behat'         => __DIR__.'/../vendor/behat/Behat/src',
+    'Behat\Gherkin'       => __DIR__.'/../vendor/behat/Gherkin/src',
+    'Behat\CommonContext' => __DIR__.'/../vendor/behat/CommonContexts',
+    'PSS'                 => __DIR__.'/../src',
+    'Knp\Bundle'          => __DIR__.'/../vendor/bundles',
+    'Zend'                => __DIR__.'/../vendor/Zend/library'
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
